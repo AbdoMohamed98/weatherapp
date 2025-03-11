c4bb568b4fcd28053363f44d15ca5a4e9d62fe1e
Abdelrahman Mohamed Ali <abdo.kaka538@gmail.com>
2025-03-11 15:05:20 +0200

    creating home page view

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(newsApp());
+}
+
+class newsApp extends StatelessWidget {
+  const newsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ,
+    );
+  }
+}
